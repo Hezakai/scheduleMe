3eb561b5d4d52ccf4fb9f1aa9c82643a952d0c0a
Eric Graves <graves.eric@gmail.com>
2023-01-09 20:01:18 -0600

    added local storage saving

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,38 +10,42 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
   $(".saveBtn").click(function () {
-    alert("Handler for " + this.parentNode.id + " .click() called.");
+    var rowID = this.parentNode.id
+    console.log("rowID: " + rowID);
+    var textID = (rowID + "txt");
+    console.log("textID: " + textID)
+    console.log($("#"+textID).val)
+    var txt = $('#'+textID).val();
+    console.log("txt = " + txt)
+    alert("Handler for " + rowID + " .click() called.");
+    localStorage.setItem(rowID, JSON.stringify(txt));
   });
-
-
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  n = 9
-  for (let i = n; i < 18; i++) {
-    y = $('#' + n)
-    console.log(y)
+
+  for (let i = 9; i < 18; i++) {
+    hourID = $('#' + i)
+    console.log(hourID)
     var currHour = (dayjs().format('H'))
-    console.log("Timeslot: " + n)
+    console.log("Timeslot: " + i)
     console.log("Current: " + currHour)
-    if (n < currHour) {
+    if (i < currHour) {
       console.log("past")
-      $(y).attr("class", "row time-block past");
+      $(hourID).attr("class", "row time-block past");
     }
-    if (n == currHour) {
+    else if (i == currHour) {
       console.log("present")
-      $(y).attr("class", "row time-block present");
+      $(hourID).attr("class", "row time-block present");
     }
-    if (n > currHour) {
+    else if (i > currHour) {
       console.log("future")
-      $(y).attr("class", "row time-block future");
+      $(hourID).attr("class", "row time-block future");
     }
-    n++
   }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
